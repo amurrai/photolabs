f8dbde0f3f932d9a3acb8d25feb4899c34b690ce
amurrai <andre.murrai@gmail.com>
2024-02-25 14:03:10 -0500

    Implement Like button in PhotoItem

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
 import "../styles/PhotoListItem.scss";
+import PhotoFavButton from "./PhotoFavButton";
 
 
 const PhotoListItem = ({data}) => {
 
   return (
     <div className="photo-list__item">
+      <PhotoFavButton />
       <img className="photo-list__image" src={data.imageSource} />
       <div className="photo-list__user-details">
         <img className="photo-list__user-profile" src={data.profile} />
