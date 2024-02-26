6e0c1373b61ac8d4ef15f38f348d6b82073b6895
amurrai <andre.murrai@gmail.com>
2024-02-26 16:58:14 -0500

    Refactor favourites function to use an array instead of object

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -4,14 +4,13 @@ import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
 function PhotoFavButton({photo, favourites, switchFavourite}) {
-  // const [selected, setSelected] = useState(false);
   const handleClick = () => {
     switchFavourite(photo.id)
   }
   return (
     <div onClick={handleClick} className="photo-list__fav-icon">
       <div className="photo-list__fav-icon-svg">
-        <FavIcon  selected={favourites[photo.id]}/>
+        <FavIcon  selected={favourites.includes(photo.id)}/>
       </div>
     </div>
   );
--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -6,16 +6,16 @@ import PhotoList from 'components/PhotoList';
 import photos from 'mocks/photos';
 
 const HomeRoute = ({photos, topics}) => {
-  const [favourites, setFavourites] = useState({});
+  const [favourites, setFavourites] = useState([]);
   const switchFavourite = (id) => {
-    setFavourites({
-      ...favourites,
-      [id]: !favourites[id]})
+    favourites.includes(id) ?
+    setFavourites(favourites.filter((value => {return value !== id}))) :
+    setFavourites([...favourites, id])
   };
 
   return (
     <div className="home-route">
-      <TopNavigation topics={topics}/>
+      <TopNavigation topics={topics} favourites={favourites}/>
       <PhotoList photos={photos} favourites={favourites} switchFavourite={switchFavourite}/>
     </div>
   );
