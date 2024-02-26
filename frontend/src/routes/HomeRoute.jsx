73fb8e05c8c1ab28cffa02fde6023ab5d4d6b5a3
amurrai <andre.murrai@gmail.com>
2024-02-26 12:25:54 -0500

    Refactor HomeRoute to destructure props

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -4,11 +4,11 @@ import '../styles/HomeRoute.scss';
 import TopNavigation from 'components/TopNavigationBar';
 import PhotoList from 'components/PhotoList';
 
-const HomeRoute = (props) => {
+const HomeRoute = ({photos, topics}) => {
   return (
     <div className="home-route">
-      <TopNavigation topics={props.topics}/>
-      <PhotoList photos={props.photos}/>
+      <TopNavigation topics={topics}/>
+      <PhotoList photos={photos}/>
     </div>
   );
 };
