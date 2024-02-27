386c9e8dfe995ed0724a969d598bcfd79b350315
amurrai <andre.murrai@gmail.com>
2024-02-26 20:06:47 -0500

    Implement blank modal for photo view

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,11 +4,13 @@ import './App.scss';
 import HomeRoute from 'routes/HomeRoute';
 import photos from 'mocks/photos';
 import topics from 'mocks/topics';
+import PhotoDetailsModal from 'routes/PhotoDetailsModal';
 
 const App = () => {
   return (
     <div className="App">
       <HomeRoute photos={photos} topics={topics}/>
+      <PhotoDetailsModal />
     </div>
   );
 };
